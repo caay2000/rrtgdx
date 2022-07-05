ee81d4698ff01325d8972f7b82d5addfbf920e2f
Albert Casanovas <caay2000@gmail.com>
2022-07-06 00:51:36 +0200

    gradle changes

---
+++ desktop/src/com/github/caay2000/rrt/DesktopLauncher.kt
@@ -0,0 +1,15 @@
+package com.github.caay2000.rrt
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+
+internal object DesktopLauncher {
+
+    @JvmStatic
+    fun main(arg: Array<String>) {
+        val config = Lwjgl3ApplicationConfiguration()
+        config.setForegroundFPS(60)
+        config.setTitle("RRTGDX")
+        Lwjgl3Application(RRTGdxGame(), config)
+    }
+}
